be8c6bbc95f0e07d81d77e30cb546bb54426f3fd
tkmagesh <tkmagesh77@gmail.com>
2015-09-03 15:22:31 +0530

    Closures & Spinner Problem Statement

--- Day-01/01-Language/src/calculator.js
+++ Day-01/01-Language/src/calculator.js
@@ -1,3 +1,24 @@
-function add(x,y){
-    return x + y;
+function add(){
+    function parseArg(n){
+        if (n instanceof Array) return add.apply(this,n);
+        if (typeof n === 'function') return parseArg(n());
+        return isNaN(n) ? 0 : parseInt(n,10);
+    }
+    return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments,1));
 }
+
+//this
+/*
+6 invocation patterns
+
+As a method of an object
+    - this -> object
+
+As a function
+    - this -> global scope -> window
+
+using the "call()" method of the function
+
+using the "apply()" method of the function
+
+*/
---
+++ Day-01/02-Closures/spinner.js
@@ -0,0 +1,22 @@
+/*Create an object and assign it to the variable "spinner" */
+
+/*
+var spinner = ...
+
+The object is expected to exhibit the following behavior
+
+spinner.up() // => 1
+spinner.up() // => 2
+spinner.up() // => 3
+spinner.up() // => 4
+
+spinner.down() //=> 3
+spinner.down() //=> 2
+spinner.down() //=> 1
+spinner.down() //=> 0
+spinner.down() //=> -1
+
+Important Note:
+The variable used to track the count should not be accessible to the outside world
+
+*/
